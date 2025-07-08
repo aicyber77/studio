84ac7e585b038c8c74242a58349ca8bcb1207d14
cyber AI <aicyber77@gmail.com>
2025-07-08 14:43:30 +0000

    este es logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,16 +5,13 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
       {...props}
     >
-      <path d="M12 2L2 7l10 5 10-5-10-5z" />
-      <path d="M2 17l10 5 10-5" />
-      <path d="M2 12l10 5 10-5" />
+      <path
+        fillRule="evenodd"
+        d="M12 22c-4-3.5-8-6.5-8-12.5a8 8 0 1 1 16 0c0 6-4 9-8 12.5z M12 10.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z m-3-0.5a3.5 3.5 0 1 1 7 0h-1.5a2 2 0 1 0-4 0h-1.5z m-3.5 0a6.5 6.5 0 0 1 6.5-6.5v1.5a5 5 0 0 0-5 5h-1.5z m6.5 6.5a6.5 6.5 0 0 1-6.5-6.5h1.5a5 5 0 0 0 5 5v1.5z"
+      />
     </svg>
   );
 }
